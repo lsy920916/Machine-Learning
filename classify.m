527b7cb146f1a0b0c3eea4a1d6cdaf161b827e8d
Liu <siyu.liu@ttec.com>
2018-03-30 16:51:47 -0400

    test

--- classify.m
+++ classify.m
@@ -9,19 +9,20 @@ y = zeros(N,1);
 v_h = zeros(1,H);
 v_o = zeros(1,O);
 
+% Weird delete
 for i = 1:N
     hog = HOG(X(i,:));
     X_hog = [hog',1];
     for j = 1:H
         v_h(j) = sigmoid(X_hog,wh(:,j));
     end
-    
+
     for j = 1:O
         v_o(j) = sigmoid(v_h,wo(:,j));
     end
-    
+
     index = find(v_o == max(v_o));
-    
+
     if(index == 10)
         y(i) = 0;
     else
